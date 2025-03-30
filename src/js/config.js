9884345eece5c84ed5ce737c17a872860fbc45ab
shawky <mastershawky2017@gmail.com>
2025-03-30 21:02:19 +0200

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '827cd979-2ddc-49f4-8aec-b5616648fca5';
+export const MODAL_CLOSE_SEC = 2.5;
